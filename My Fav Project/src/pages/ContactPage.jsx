91623cc212b312541b5367dc777c16f011d73550
ravanabdinli <rabdinli17966@ada.edu.az>
2025-01-06 16:55:41 +0400

    My Favorite Project so far

---
+++ My Fav Project/src/pages/ContactPage.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./ContactPage.css"; // Import the CSS file
+
+const ContactPage = () => {
+  const [formData, setFormData] = useState({
+    subject: "",
+    email: "",
+    message: "",
+  });
+
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/messages", formData)
+      .then(() => {
+        setSuccessMessage("Message sent successfully!");
+        setErrorMessage("");
+        setFormData({ subject: "", email: "", message: "" }); // Reset form
+      })
+      .catch(() => {
+        setErrorMessage("Failed to send the message. Please try again.");
+        setSuccessMessage("");
+      });
+  };
+
+  return (
+    <div className="contact-container">
+      <h1>📧Contact Us📧</h1>
+      <form onSubmit={handleSubmit} className="contact-form">
+        <div className="form-group">
+          <label htmlFor="subject">Subject:</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            value={formData.subject}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleInputChange}
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Send <br />📩</button>
+      </form>
+      {successMessage && <p className="success-message">{successMessage}</p>}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default ContactPage;
